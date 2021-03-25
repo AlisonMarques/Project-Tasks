a14beb1e01253c964db14c9fd7389bd80c46f6d8
AlisonMarques <alison-marques@outlook.com.br>
2021-03-25 09:49:12 -0300

    Adicionado o botão de adcionar tarefa.

--- src/screens/TaskList.js
+++ src/screens/TaskList.js
@@ -26,7 +26,7 @@ export default class TaskList extends Component {
   //criando um estado para criar a funcionalidade do scroll
   state = {
     showDoneTasks: true,
-    showAddTask: true,
+    showAddTask: false,
     visibleTasks: [],
     tasks: [
       {
@@ -116,6 +116,12 @@ export default class TaskList extends Component {
             )}
           />
         </View>
+        <TouchableOpacity
+          style={styles.addButton}
+          activeOpacity={0.7}
+          onPress={() => this.setState({ showAddTask: true })}>
+          <Icon name="plus" size={20} color={commonStyles.colors.secondary} />
+        </TouchableOpacity>
       </View>
     );
   }
@@ -157,5 +163,16 @@ const styles = StyleSheet.create({
     marginHorizontal: 20,
     justifyContent: 'flex-end',
     marginTop: Platform.OS === 'ios' ? 40 : 10,
+  },
+  addButton: {
+    position: 'absolute',
+    right: 30,
+    bottom: 30,
+    width: 50,
+    height: 50,
+    borderRadius: 25,
+    backgroundColor: commonStyles.colors.today,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
